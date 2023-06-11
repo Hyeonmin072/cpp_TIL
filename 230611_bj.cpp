a7384e44d4185543c22ad4a020cfefdec6ac00c9
Hyeonmin072 <Hyeonmin072@users.noreply.github.com>
2023-06-11 20:11:45 +0900

    230611_bj

--- 230611_bj.cpp
+++ 230611_bj.cpp
@@ -1,13 +1,61 @@
-#include <iostream>
-using namespace std;
-#include<string>
+// // // #include <iostream>
+// // // using namespace std;
+// // // #include<string>
 
-int main()
-{
-	string str;
-	int index;
+// // // int main()
+// // // {
+// // // 	string str;
+// // // 	cin >> str;
 
-	cin >> str;
-	cin >> index;
-	cout << str.at(index);
-}
+// // // 	int length = str.length();
+
+// // // 	cout << length;
+	
+// // // }
+
+// //ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+// // #include <iostream>
+// // using namespace std;
+// // #include <string>
+
+
+// // int main()
+// // {
+// // 	int test;
+// // 	string _case;
+// // 	string sum;
+// // 	cin >> test;
+
+// // 	for (int i = 0; i < test; i++)
+// // 	{
+// // 		cin >> _case;
+// // 		sum += _case.front();
+// // 		sum += _case.back();
+// // 	}
+
+// // 	for (int i = 0; i < test * 2; i++)
+// // 	{
+// // 		cout << sum[i];
+// // 		if (i % 2 == 1)
+// // 		{
+// // 			cout << endl;
+// // 		}
+// // 	}
+// // }
+
+// //ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+// #include <iostream>
+// using namespace std;
+// #include <string>
+
+
+// int main()
+// {
+// 	char a;
+// 	cin >> a;
+	
+
+// 	int b = int(a);
+
+// 	cout << b;
+// }
