6053ec7897ca4bf2cc0df03100db481f844e800e
Hyeonmin072 <Hyeonmin072@users.noreply.github.com>
2024-05-06 18:12:55 +0900

    240506

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int y, m, d, sum;
+
+	scanf("%d %d %d",&y, &m, &d);
+	sum = y + m + d;
+
+	if (sum % 10 == 0)
+		printf("운수대통");
+	else
+		printf("노력하세요");
+
+}
